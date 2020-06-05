c2d9cab42f85295815893152060f8b33be2529bb
Jeong Man <jung951213@gmail.com>
2020-06-05 17:31:57 +0900

    서버 업로드
    2020/05/28 아직 미완성된 요소가 있지만 네트워크 연동 작업 전에 업로드

---
+++ Server/G_Server/G_Server/MyInclude.h
@@ -0,0 +1,22 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <WinSock2.h>
+#include <Windows.h>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <mutex>
+#include <thread>
+#include <random>
+#include <queue>
+#include <chrono>
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::mutex;
+using std::vector;
+using std::map;
+
+using namespace std::chrono;
