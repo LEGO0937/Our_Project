0fd978fc655a47a47ed350ceceecf62fc54d8310
Kim Young Wan <powerkyw59@naver.com>
2020-07-14 13:47:25 +0900

    로딩 화면 추가
    씬객체의 빌드 시간이 5~6초를 넘어가면서 빌드도중에 화면이 멈춰있는것처럼 보이기때문에 빌드 도중에 로딩중임을 나타내는 씬을 하나 더 만들었음 이는 빌드 중일때만 스프라이트를 랜더링을하게하여 로딩중임을 나타낸다.
    구현은 완료한 상태이며 리소스만  만들면 적용할 예정

---
+++ Client/DinoRun/DinoRun/CShaders/ModelShader/MeteoriteShader.cpp
@@ -0,0 +1,93 @@
+#include "ModelShader.h"
+#include "../../Common//FrameWork/CreateManager.h"
+#include "ItemObject.h"
+
+MeteoriteShader::MeteoriteShader()
+{
+	shaderName = "MeteoriteShader";
+	isFixedUpdate = true;
+}
+MeteoriteShader::~MeteoriteShader()
+{
+
+}
+
+void MeteoriteShader::Load(CreateManager* pCreateManager, const char* filename, const char* Loadname)
+{
+	billBoardCb->fSize = 50;
+	isEnable = true;
+
+	FILE *pInFile = NULL;
+	::fopen_s(&pInFile, Loadname, "rb");
+	if (!pInFile)
+		return;
+	ItemMeteorite* pItemObject = NULL;
+	UINT nReads;
+	int nLength = 0;
+
+	const char* fileName = instancingModelName.c_str();
+
+	nReads = (UINT)::fread(&nLength, sizeof(int), 1, pInFile);
+	for (int i = 0; i < nLength; ++i)
+	{
+		CLoadedModelInfo *pModel = CGameObject::LoadGeometryAndAnimationFromFile(pCreateManager, fileName, NULL);
+		pItemObject = new ItemMeteorite;
+		pItemObject->SetChild(pModel->m_pModelRootObject->GetChild());
+		pItemObject->AddRef();
+
+		//이곳에서 findFrame을 통해 각 오브젝트에 질량 및 키네마틱 값 추가할 것.
+		nReads = (UINT)::fread(&(pItemObject->m_xmf4x4ToParent), sizeof(XMFLOAT4X4), 1, pInFile);
+		pItemObject->SetId(m_iCurSerealNum++);
+
+		objectList.emplace_back(pItemObject);
+		if (pModel)
+		{
+			delete pModel;
+			pModel = NULL;
+		}
+	}
+
+	::fclose(pInFile);
+}
+
+void MeteoriteShader::Update(float fTimeElapsed)
+{
+	// 서버로부터 추가 신호를 받고 addObject(m_pCreateManager.get(), matrix)호출
+	// 제거 명령도 여기서 받고 삭제할 것. 접근 방법은 아이디로 찾을 것.
+	if (objectList.size())
+	{
+		for (vector<CGameObject*>::iterator it = objectList.begin(); it != objectList.end();)
+		{
+			if ((*it)->Update(fTimeElapsed, NULL))
+			{
+				(*it)->Release();
+				it = objectList.erase(it);
+			}
+			else
+				it++;
+		}
+	}
+}
+
+void MeteoriteShader::addObject(CreateManager* pCreateManager, const XMFLOAT4X4& xmf3DepartPosition, const XMFLOAT4X4& xmf3ArrivePosition)
+{
+	ItemMeteorite* pItemObject = NULL;
+	CLoadedModelInfo *pModel = CGameObject::LoadGeometryAndAnimationFromFile(pCreateManager, instancingModelName.c_str(), NULL);
+
+	pItemObject = new ItemMeteorite;
+	pItemObject->SetChild(pModel->m_pModelRootObject->GetChild());
+	pItemObject->AddRef();
+
+	pItemObject->SetMatrix(xmf3DepartPosition);
+	pItemObject->SetUpdatedContext(m_pUpdatedContext);
+	pItemObject->SetId(m_iCurSerealNum++);
+
+	objectList.emplace_back(pItemObject);
+	if (pModel)
+	{
+		delete pModel;
+		pModel = NULL;
+	}
+	ReleaseShaderVariables();
+	CreateShaderVariables(pCreateManager);
+}
---
+++ Client/DinoRun/DinoRun/Objects/ItemObject.h
@@ -0,0 +1,83 @@
+#pragma once
+#include "BaseObject.h"
+#include "EventHandler/EventHandler.h"
+
+class ItemObject : public CGameObject
+{
+private:
+	float m_fEnableCount = 0;
+public:
+	virtual bool Update(float fTimeElapsed, CGameObject* target);
+
+	ItemObject(int nMeshes = 1);
+	~ItemObject();
+};
+
+class ItemMeat : public CGameObject
+{
+private:
+	float m_fEnableCount = 0;
+public:
+	virtual bool Update(float fTimeElapsed, CGameObject* target);
+
+	ItemMeat(int nMeshes = 1);
+	~ItemMeat();
+};
+
+class ItemBanana : public CGameObject
+{
+private:
+	float m_fLifeCount = 0;
+public:
+	virtual bool Update(float fTimeElapsed, CGameObject* target);
+
+	ItemBanana(int nMeshes = 1);
+	~ItemBanana();
+};
+
+class ItemMud : public CGameObject
+{
+private:
+	float m_fLifeCount = 0;
+public:
+	virtual bool Update(float fTimeElapsed, CGameObject* target);
+
+	ItemMud(int nMeshes = 1);
+	~ItemMud();
+};
+
+
+class ItemStone : public CGameObject
+{
+private:
+	float m_fLifeCount = 0;
+public:
+	virtual bool Update(float fTimeElapsed, CGameObject* target);
+
+	ItemStone(int nMeshes = 1);
+	~ItemStone();
+};
+
+
+class ItemMeteorite : public CGameObject
+{
+private:
+	float m_fLifeCount = 0;
+public:
+	virtual bool Update(float fTimeElapsed, CGameObject* target);
+	virtual void OnUpdateCallback(float fTimeElapsed);
+
+	ItemMeteorite(int nMeshes = 1);
+	~ItemMeteorite();
+};
+
+class ItemMound : public CGameObject
+{
+private:
+	float m_fLifeCount = 0;
+public:
+	virtual bool Update(float fTimeElapsed, CGameObject* target);
+
+	ItemMound(int nMeshes = 1);
+	~ItemMound();
+};
