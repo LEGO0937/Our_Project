4aab37039bfbd6c58d8a9a5c5446b40404e961ec
LEGO0937 <39212289+LEGO0937@users.noreply.github.com>
2020-07-27 20:30:22 +0900

    WM_SOCKET정의 까먹은거 업로드,
    WM_SOCKET정의 까먹은거 업로드

--- DinoRun/DinoRun/DinoRun.cpp
+++ DinoRun/DinoRun/DinoRun.cpp
@@ -2,6 +2,7 @@
 //
 #include "DinoRun.h"
 #define MAX_LOADSTRING 100
+#define WM_SOCKET WM_USER + 1
 
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
